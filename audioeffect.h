58aef3d0d38b3d6122cd4cc24ec5eeed1e80ebaa
kalalnara103-ai <kalalnara103@gmail.com>
2026-05-11 14:31:12 +0300

    Update audioeffect.h

--- audioeffect.h
+++ audioeffect.h
@@ -1,15 +1,20 @@
 #ifndef __audioeffect__
 #define __audioeffect__
 #include "aeffect.h"
+
 class AudioEffect {
 public:
     AudioEffect(audioMasterCallback audioMaster, VstInt32 numP, VstInt32 numParams) {
-        memset(&aeffect, 0, sizeof(aeffect)); aeffect.magic = 0x56737450; aeffect.numPrograms = numP; aeffect.numParams = numParams;
+        memset(&aeffect, 0, sizeof(aeffect));
+        aeffect.magic = 0x56737450;
+        aeffect.numPrograms = numP;
+        aeffect.numParams = numParams;
+        curProgram = 0; // تعريف المتغير اللي مسبب مشكلة
     }
     virtual ~AudioEffect() {}
     virtual void setParameter(VstInt32 index, float value) {}
     virtual float getParameter(VstInt32 index) { return 0; }
-    virtual void setProgram(VstInt32 program) {}
+    virtual void setProgram(VstInt32 program) { curProgram = program; }
     virtual void setProgramName(char* name) {}
     virtual void getProgramName(char* name) {}
     virtual void getParameterLabel(VstInt32 index, char* label) {}
@@ -24,11 +29,15 @@ public:
     virtual bool getProgramNameIndexed(VstInt32 category, VstInt32 index, char* text) { return false; }
     AEffect* getAeffect() { return &aeffect; }
 protected:
-    AEffect aeffect; float sampleRate; VstInt32 curProgram;
+    AEffect aeffect;
+    float sampleRate;
+    VstInt32 curProgram; // المتغير هنا
 };
+
 class AudioEffectX : public AudioEffect {
 public:
-    AudioEffectX(audioMasterCallback audioMaster, VstInt32 numP, VstInt32 numParams) : AudioEffect(audioMaster, numP, numParams) {}
+    AudioEffectX(audioMasterCallback audioMaster, VstInt32 numP, VstInt32 numParams) 
+    : AudioEffect(audioMaster, numP, numParams) {}
     void setSampleRate(float rate) { AudioEffect::setSampleRate(rate); }
 };
 #endif
